a02623fd1da1ec50f4ee6242ffdeea97fb092d0e
Rafetoledo <123509599+Rafetoledo@users.noreply.github.com>
2023-05-26 17:29:42 -0300

    Adicionando Messages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { FormsModule } from '@angular/forms';
+import { heroDetailComponent } from './hero-detail/hero-detail.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatExpansionModule} from '@angular/material/expansion';
+import { MessagesComponent } from './messages/messages.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    heroDetailComponent,
+    MessagesComponent,
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/mock-hero.ts
@@ -0,0 +1,10 @@
+import { Hero } from "./heroes/hero.model";
+
+ export const HEROES: Hero[] = [
+  {id: 1, name: 'Wolverine'},
+  {id: 2, name: 'Homem-Aranha'},
+  {id: 3, name: 'Magneto'},
+  {id: 4, name: 'The Flash'},
+  {id: 5, name: 'Tempestade'},
+  {id: 6, name: 'Venom'}
+];
